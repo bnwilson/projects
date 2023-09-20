c064ac8a4ac5545aa7a02d45559baa27d55439ba
bnwilson <bwilson.projects@gmail.com>
2023-09-20 17:28:58 -0400

    added additional arg validation steps

--- scripts/iconify.sh
+++ scripts/iconify.sh
@@ -4,22 +4,40 @@
 ###  example:
 ###  	"./iconify.sh image_name.png"
 ###  creates an "icons/oldIcons/" file and copies original to that location
+###
+### Requirements:
+###		imagemagick
+###
+### Download & installation info can be found below:
+### 	- https://imagemagick.org/script/download.php
+### 	- win10, linux, & macOS
 
-# Image name as first arg
+# Image-file name as first arg
 IMAGE=$1
+
+# Constant variables
 ICONFILE="icons"
 OLDICONS="$ICONFILE/old_icons"
 
-# Check if arg was made
+# Exit 16 - filename not given (no arg passed)
 if [ -z "$IMAGE" ];then
-	echo "Must pass a file name in the current directory or FQN"
+	echo "Must pass a file name in the current directory or FQN\n   ex. './iconify.sh original_image.png'"
 	exit 16
 fi
 
-# Check if file existscurrent directory
+# Exit 17 - if arg file does not exist
 if [ ! -f "$IMAGE" ]; then 
 	echo "This file doesn't exist"
-	exit 16
+	exit 17
+fi
+
+# Exit 18 - not a valid image file extension
+if [[ "$IMAGE" =~ \.(bmp|jpg|tif|tiff|png|gif|jpeg)$ ]]; then
+	echo "File does have a valid extension, continuing ..."
+	echo "  ..."
+else
+	echo "Error 18:  the file '$IMAGE' does NOT have a valid image-file-extension (.bmp, .jpg, etc.)"
+	exit 18
 fi
 
 if [ ! -e "$ICONFILE" ]; then
